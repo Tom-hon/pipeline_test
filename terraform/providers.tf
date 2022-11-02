9020e483d7ea537f546982d6da2d12c272dae8cc
Tom H <tomhoneywellv@hotmail.com>
2022-11-02 22:47:39 +0000

    go for it

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,21 @@
-provider "azurerm" {
-  # resource_group_name  = "tfstate"
-  # storage_account_name = "tfstatehere"
-  # container_name       = "tfstate"
-  # key                  = "terraform.tfstate"
-  # access_key           = "c3+9DG/bYoU6ZC1DOpJDA3QQ6DFvdSYG3/XEvO2EWE8S3vzQGydQHkpU0IjHPKyu6xgQN7o/Q/mG+AStdfHm8g=="
-  features {}
+# provider "azurerm" {
+#   # resource_group_name  = "tfstate"
+#   # storage_account_name = "tfstatehere"
+#   # container_name       = "tfstate"
+#   # key                  = "terraform.tfstate"
+#   # access_key           = "c3+9DG/bYoU6ZC1DOpJDA3QQ6DFvdSYG3/XEvO2EWE8S3vzQGydQHkpU0IjHPKyu6xgQN7o/Q/mG+AStdfHm8g=="
+#   features {}
+# }
+
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.0"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
 }
--- terraform/tftest.tf
+++ terraform/tftest.tf
@@ -1,16 +1,28 @@
-resource "azurerm_resource_group" "test_storage" {
-  name     = var.test_storage
-  location = var.location
-}
+# resource "azurerm_resource_group" "test_storage" {
+#   name     = var.test_storage
+#   location = var.location
+# }
 
-resource "azurerm_storage_account" "example" {
-  name                     = var.test_storage
-  resource_group_name      = azurerm_resource_group.test_storage.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# resource "azurerm_storage_account" "example" {
+#   name                     = var.test_storage
+#   resource_group_name      = azurerm_resource_group.test_storage.name
+#   location                 = var.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = var.tags
+#   tags = {
+#     environment = var.tags
+#   }
+# }
+
+resource "null_resource" "random" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command     = "Get-Date"
+    interpreter = ["PowerShell", "-Command"]
   }
 }
