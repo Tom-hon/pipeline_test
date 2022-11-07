2fc79cfda7b6ab49472595384fdcc57015fcef7f
Tom H <tomhoneywellv@hotmail.com>
2022-11-07 10:13:54 +0000

    adding for usage

--- terraform/tftest.tf
+++ terraform/tftest.tf
@@ -1,19 +1,20 @@
-# resource "azurerm_resource_group" "test_storage" {
-#   name     = var.test_storage
-#   location = var.location
-# }
+resource "azurerm_resource_group" "test_storage" {
+  name     = var.test_storage
+  location = var.location
+}
 
-# resource "azurerm_storage_account" "example" {
-#   name                     = var.test_storage
-#   resource_group_name      = azurerm_resource_group.test_storage.name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
+resource "azurerm_storage_account" "example" {
+  for_each                 = { for item in local.instances : item.DestinationIP => item }
+  name                     = each.value.ModelFlow
+  resource_group_name      = azurerm_resource_group.test_storage.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-#   tags = {
-#     environment = var.tags
-#   }
-# }
+  tags = {
+    environment = var.tags
+  }
+}
 
 resource "null_resource" "random" {
 
